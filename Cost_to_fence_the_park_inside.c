1a923eccada68cafa87fb6dcff0b4ab1cc56b467
Mounika <98006272+Mounikachowdar@users.noreply.github.com>
2022-01-30 22:46:58 -0800

    #include<stdio.h> int main() {     int l,b,w,c,a;     scanf("%d%d%d%d",&l,&b,&w,&c);     if(l<=2*w||b<=2*w)     {         printf("Impossible");     }     else     {         a=(l*b-(l-2*w)*(b-2*w));         printf("%d",a*c);     }     return 0; }

---
+++ Cost_to_fence_the_park_inside.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,w,c,a;
+    scanf("%d%d%d%d",&l,&b,&w,&c);
+    if(l<=2*w||b<=2*w)
+    {
+        printf("Impossible");
+    }
+    else
+    {
+        a=(l*b-(l-2*w)*(b-2*w));
+        printf("%d",a*c);
+    }
+    return 0;
+}
